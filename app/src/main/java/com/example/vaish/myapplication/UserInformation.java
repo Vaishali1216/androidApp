f177b9b552eaea7803ff2f8cbc9c641d932e6506
chrnndz3 <chrnndz3@gmail.com>
2017-04-11 18:20:13 -0500

    adding database files

---
+++ app/src/main/java/com/example/vaish/myapplication/UserInformation.java
@@ -0,0 +1,36 @@
+package com.example.vaish.myapplication;
+
+/**
+ * Created by twiganator on 4/11/17.
+ */
+
+public class UserInformation {
+
+    int id;
+    String username;
+    String password;
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+}
